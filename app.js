8c0e266529a2a555217c77f93a15bc68f76d7e99
jishu91 <120646662+jishu91@users.noreply.github.com>
2025-12-01 21:33:43 +0530

    middleware

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
