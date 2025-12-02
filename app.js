49ab250deae09ff7805c5c1e821c1c5988056f1e
jishu91 <120646662+jishu91@users.noreply.github.com>
2025-12-02 13:31:50 +0530

    ejs

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.render('index');
 });
 
 app.listen(3000);
