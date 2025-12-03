fa75ff9894cbc3fd5f4aed53f09aff008714f971
jishu91 <120646662+jishu91@users.noreply.github.com>
2025-12-03 06:52:15 +0530

    to do list complete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -8,7 +9,34 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    fs.readdir(`./files`, function(err, files){
+        res.render("index", {files: files});
+    }) 
 });
+
+app.post('/create', (req, res) => {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details, (err)=>{
+        res.redirect('/')
+    })
+});
+
+app.get('/files/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,filedata)=>{
+        res.render('show',{filename: req.params.filename, filedata:filedata})
+    })
+});
+
+app.get('/edit/:filename', (req, res) => {
+    res.render('edit',{filename:req.params.filename})
+});
+
+app.post('/edit/', (req, res) => {
+    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`, (err)=>{
+        res.redirect('/')
+    })
+});
+
+
+
 
 app.listen(3000);
